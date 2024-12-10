0db44322f118e778eec70e45f025137902617c03
Clarissa De Benedittis <debenedittisclarissa04@gmail.com>
2024-12-10 20:04:57 +0100

    commit

---
+++ New-React-App-2/src/App.jsx
@@ -0,0 +1,23 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  const massage = [
+    {
+      id: 1,
+      text: "Ciao",
+      inviato: true,
+    },
+    {
+      id: 1,
+      text: "Ciao",
+      inviato: true,
+    },
+  ];
+
+  const [messages, setMassages] = useState(0);
+
+  return <></>;
+}
+
+export default App;
